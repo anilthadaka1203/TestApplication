d1c1d84b709188714b1bae4d5269a649b8b68885
anilthadaka1203 <anil.anni4006@gmail.com>
2019-02-04 16:54:45 +0530

    Added View

--- app/src/main/java/com/fcm/vf/testapplication/MainActivity.java
+++ app/src/main/java/com/fcm/vf/testapplication/MainActivity.java
@@ -2,12 +2,17 @@ package com.fcm.vf.testapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String a;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
